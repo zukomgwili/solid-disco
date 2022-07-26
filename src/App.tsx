f7353df5dfa5ed160d783b3c0516e18bc6875eef
Zuko Mgwili <zukomgwili@gmail.com>
2022-07-26 17:32:54 +0100

    chore: add react flow component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ FC } from 'react';
+import ReactFlow, { MiniMap, Controls, ReactFlowProps } from 'react-flow-renderer';
 
-function App() {
+const App: FC = ({ nodes, edges, onNodesChange, onEdgesChange, onConnect }:ReactFlowProps) =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+    >
+      <MiniMap />
+      <Controls />
+    </ReactFlow>
   );
 }
 
