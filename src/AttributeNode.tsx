d194a693e8d69382ba498d865558e17fc183a88d
Zuko Mgwili <zukomgwili@gmail.com>
2022-07-31 15:07:51 +0100

    feat: apply styles to the attribute node

--- src/AttributeNode.tsx
+++ src/AttributeNode.tsx
@@ -1,18 +1,25 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import { Handle, Position } from 'react-flow-renderer';
 
 interface AttributeNodeProps {
   data: { name: string };
 }
 
-const AttributeNode: FC<AttributeNodeProps> = ({ data: { name } }) => (
-  <article>
-    <section>
-      <header>{name}</header>
-      <input type="range" step="0.01" min="0" max="1" />
-    </section>
-    <Handle type="source" position={Position.Bottom} />
-  </article>
-);
+const AttributeNode: FC<AttributeNodeProps> = ({ data: { name } }) => {
+  const [value, setValue] = useState(1);
+  const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(parseFloat(event.target.value));
+  }, []);
 
+  return (
+    <article className="attribute-node">
+      <header>{name}</header>
+      <section className="weight">
+        <header>{`Weight: ${value}`}</header>
+        <input type="range" step="0.01" min="0" max="1" value={value} onChange={handleChange} />
+      </section>
+      <Handle type="source" position={Position.Bottom} />
+    </article>
+  );
+};
 export default AttributeNode;
